6418f1db669c0dc489117e5f1f7852424b9c32c8
mcoenca <m.coenca@gmail.com>
2015-04-11 16:42:54 +0200

    having fun with parallax and other stuff

--- WebApp/app/client/templates/layouts/master_layout/master_layout.js
+++ WebApp/app/client/templates/layouts/master_layout/master_layout.js
@@ -2,7 +2,12 @@ Template.MasterLayout.helpers({
 });
 
 Template.MasterLayout.events({
-    'click #logout-button':function() {
+    'click .logout-button':function() {
         AccountsTemplates.logout();
     }
 });
+
+Template.MasterLayout.rendered = function () {
+    $(".button-collapse").sideNav();
+    $('.parallax').parallax();
+};
